cc35fab54d3e88f01eb007b92ced1ef1af2ca29b
Rochan Sabek <rms134@mail.aub.edu>
2026-05-29 23:22:47 +0300

    Add authentication DTOs

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bf8c6d6a37db49f9b03cd592a2d334b698e8777")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c737519738084b8dad9768951af5c5477295f34")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
